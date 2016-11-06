f33172e3f5468484065c7114456390c78f8f2f57
Thomas Darimont <thomas.darimont@gmail.com>
2016-11-06 13:55:51 +0100

    Add WebListener example

---
+++ asw2016-demo/asw2016-demo-webapp/src/main/java/de/asw/courses/web2016/demo/backend/servlet/WebListenerExample.java
@@ -0,0 +1,24 @@
+package de.asw.courses.web2016.demo.backend.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.jboss.logging.Logger;
+
+@WebListener
+public class WebListenerExample implements ServletContextListener {
+
+	Logger log = Logger.getLogger(WebListenerExample.class);
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		log.info("Context Initalized");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		log.info("Context Destroyed");
+	}
+
+}
