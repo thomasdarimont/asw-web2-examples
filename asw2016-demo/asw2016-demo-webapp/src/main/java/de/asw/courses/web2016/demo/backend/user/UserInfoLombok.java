3e00d49621e50304a9a2729122fd5aac141d4adc
Thomas Darimont <thomas.darimont@gmail.com>
2016-11-06 12:58:00 +0100

    Add projectlombok

---
+++ asw2016-demo/asw2016-demo-webapp/src/main/java/de/asw/courses/web2016/demo/backend/user/UserInfoLombok.java
@@ -0,0 +1,19 @@
+package de.asw.courses.web2016.demo.backend.user;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class UserInfoLombok {
+
+	String firstname;
+	String lastname;
+	String email;
+}
