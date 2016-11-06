a06ab5c1301ad1ae045dfe83361ad3aa0d842638
Thomas Darimont <thomas.darimont@gmail.com>
2016-11-06 16:00:40 +0100

    Http Session example

---
+++ asw2016-demo/asw2016-demo-webapp/src/main/java/de/asw/courses/web2016/demo/backend/servlet/HttpSessionServletExample.java
@@ -0,0 +1,54 @@
+package de.asw.courses.web2016.demo.backend.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * <pre>
+ * {@code http://localhost:8080/asw2016-demo-webapp/session }
+ * </pre>
+ * 
+ * <pre>
+ * {@code http://localhost:8080/asw2016-demo-webapp/session?name=test }
+ * </pre>
+ * 
+ * <pre>
+ * {@code http://localhost:8080/asw2016-demo-webapp/session?kill }
+ * </pre>
+ * 
+ * Chrome -> Developer Tools -> Application -> Cookies
+ * 
+ * @author tom
+ *
+ */
+@WebServlet("/session")
+public class HttpSessionServletExample extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HttpSession session = req.getSession();
+
+		if (req.getParameterMap().containsKey("kill")) {
+			session.invalidate();
+			resp.getWriter().printf("Hello %s%n", (String) null);
+			return;
+		}
+
+		String name = req.getParameter("name");
+
+		if (name == null) {
+			name = (String) session.getAttribute("name");
+		} else {
+			session.setAttribute("name", name);
+		}
+
+		resp.getWriter().printf("Hello %s%n", name);
+	}
+}
