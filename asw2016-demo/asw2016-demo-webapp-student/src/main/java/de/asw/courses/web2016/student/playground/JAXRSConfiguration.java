e19cadfa770854225a4feacba12ef978e3408ed0
Thomas Darimont <thomas.darimont@gmail.com>
2016-11-30 20:56:04 +0100

    Add new web app project for student experiments

---
+++ asw2016-demo/asw2016-demo-webapp-student/src/main/java/de/asw/courses/web2016/student/playground/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package de.asw.courses.web2016.student.playground;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+	// Verwenden wir sobald wir zu JAX-RS kommen...
+}
---
+++ asw2016-demo/asw2016-demo-webapp-student/src/main/java/de/asw/courses/web2016/student/playground/servlets/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package de.asw.courses.web2016.student.playground.servlets;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <ol>
+ * <li>Leiten Sie die Klasse {@link HelloWorldServlet} von der Klasse
+ * {@link HttpServlet} ab.</li>
+ * <li>Überschreiben Sie anschließend die Methode
+ * {@link #doGet(HttpServletRequest, HttpServletResponse)}</li>
+ * <li>Verwenden Sie das Response Writer Objekt der {@link HttpServletResponse}
+ * um mittels {@link PrintWriter#println()}
+ * {@code "Hello World! " + LocalDateTime.now()} in der Response zu senden.</li>
+ * </ol>
+ * 
+ * @author tom
+ */
+public class HelloWorldServlet /* hier von Klasse erben */ {
+
+	// Hier doGet implementieren
+}
