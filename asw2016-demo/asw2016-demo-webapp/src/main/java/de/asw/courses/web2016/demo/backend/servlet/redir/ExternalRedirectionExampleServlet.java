f755fb4b3d11a90f36c9ff2f9dcd53806510fa7a
Thomas Darimont <thomas.darimont@gmail.com>
2016-12-01 10:47:56 +0100

    Add redirection examples

---
+++ asw2016-demo/asw2016-demo-webapp/src/main/java/de/asw/courses/web2016/demo/backend/servlet/redir/ExternalRedirectionExampleServlet.java
@@ -0,0 +1,25 @@
+package de.asw.courses.web2016.demo.backend.servlet.redir;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * {@code http://localhost:8080/asw2016-demo-webapp/redirect?redirect_uri=http://www.google.de }
+ * @author tom
+ *
+ */
+@WebServlet("/redirect")
+public class ExternalRedirectionExampleServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String redirectUri = req.getParameter("redirect_uri");
+		resp.sendRedirect(redirectUri); // sends a 302 Temporary Redirect
+	}
+}
