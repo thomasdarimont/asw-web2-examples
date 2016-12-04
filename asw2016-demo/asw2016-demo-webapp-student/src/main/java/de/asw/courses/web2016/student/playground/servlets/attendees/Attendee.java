fa66b4b5a6aa45275efdc78cfbf3e680e10051be
Thomas Darimont <thomas.darimont@gmail.com>
2016-12-04 15:33:20 +0100

    Add additional examples for form processing with servlets

---
+++ asw2016-demo/asw2016-demo-webapp-student/src/main/java/de/asw/courses/web2016/student/playground/servlets/attendees/Attendee.java
@@ -0,0 +1,63 @@
+package de.asw.courses.web2016.student.playground.servlets.attendees;
+
+import java.io.Serializable;
+
+public class Attendee implements Serializable {
+
+	String name;
+	String email;
+
+	public Attendee() {
+	}
+
+	public Attendee(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Attendee [name=" + name + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Attendee other = (Attendee) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+}
