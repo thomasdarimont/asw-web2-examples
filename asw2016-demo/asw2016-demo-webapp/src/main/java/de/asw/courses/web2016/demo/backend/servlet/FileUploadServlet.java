9bf95740645bcc42f51ab2b67592af56286e0bb3
Thomas Darimont <thomas.darimont@gmail.com>
2016-12-05 22:09:34 +0100

    Add file upload example

---
+++ asw2016-demo/asw2016-demo-webapp/src/main/java/de/asw/courses/web2016/demo/backend/servlet/FileUploadServlet.java
@@ -0,0 +1,46 @@
+package de.asw.courses.web2016.demo.backend.servlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet(urlPatterns = "/upload", //
+		initParams = @WebInitParam(name = "location", value = "/tmp") //
+)
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, // 10 MB
+		maxFileSize = 1024 * 1024 * 50, // 50 MB
+		maxRequestSize = 1024 * 1024 * 100 // 100 MB
+		) 
+public class FileUploadServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		Part filePart = req.getPart("file");
+
+		String uploadLocation = getInitParameter("location");
+
+		File file = new File(uploadLocation, filePart.getSubmittedFileName());
+		try (//
+				ReadableByteChannel in = Channels.newChannel(filePart.getInputStream()); //
+				FileOutputStream out = new FileOutputStream(file)//
+		) {
+			out.getChannel().transferFrom(in, 0, filePart.getSize());
+		}
+
+		resp.getWriter().println("Upload completed: " + file.getAbsolutePath());
+	}
+}
---
+++ asw2016-demo/asw2016-demo-webapp/src/main/webapp/upload.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+ <h1>File Upload</h1>
+ <!-- See de.asw.courses.web2016.demo.backend.servlet.FileUploadServlet -->
+ <form action="upload" method="POST" enctype="multipart/form-data">
+  <label for="file">Datei: </label>
+  <input type="file" name="file" id="file"></input>
+  <button>Upload</button>
+ </form>
+</body>
+</html>
