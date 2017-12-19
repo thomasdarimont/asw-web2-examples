fd787048eab0a884d3380f07765947c7f19889af
Thomas Darimont <thomas.darimont@gmail.com>
2017-12-19 09:20:03 +0100

    Revise manual injection example

--- asw-web-2-training-java/java-snippets/src/main/java/de/asw/java/training2016/di/ManualDependenciesExample.java
+++ asw-web-2-training-java/java-snippets/src/main/java/de/asw/java/training2016/di/ManualDependenciesExample.java
@@ -1,33 +1,41 @@
 package de.asw.java.training2016.di;
 
+class Komponente1 {
+
+	Komponente2 komponente2;
+
+	public Komponente2 getKomponente2() {
+		return komponente2;
+	}
+
+	public void setKomponente2(Komponente2 komponente2) {
+		this.komponente2 = komponente2;
+	}
+
+	void someOperation() {
+		System.out.println("Komponente1.someOperation()");
+		System.out.println("Komponente2: " + komponente2.toString());
+	}
+}
+
+class Komponente2 {
+	// ...
+}
+
 public class ManualDependenciesExample {
-	
-	static class Komponente1{
-		//...
-		Komponente2 komponente2;
 
-		public Komponente2 getKomponente2() {
-			return komponente2;
-		}
-
-		public void setKomponente2(Komponente2 komponente2) {
-			this.komponente2 = komponente2;
-		}
-		
-		void someOperation(){
-			System.out.println("ManualDependenciesExample.Komponente1.someOperation()");
-			System.out.println("Komponente2: " + komponente2.toString());
-		}
-	}
-	
-	static class Komponente2{
-		//...
-	}
-	
 	public static void main(String[] args) {
-		
+
+		// Manuelle Instanziierung von Komponente1
 		Komponente1 k1 = new Komponente1();
-		k1.setKomponente2(new Komponente2());
+
+		// Manuelle Instanziierung von Komponente2
+		Komponente2 k2 = new Komponente2();
+
+		// Manuelle Konfiguration von Komponente1
+		k1.setKomponente2(k2);
+
+		// Komponente1 ist jetzt verwendbar
 		k1.someOperation();
 	}
 }
